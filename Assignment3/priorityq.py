3ca5b827607f5627cdc998f83d2fd228812b150a
Vaibhavi <vaibhavi.rangarajan@gmail.com>
2017-10-18 18:16:39 -0700

    Changed priorityq implementation to avoid double deletion

--- Assignment3/priorityq.py
+++ Assignment3/priorityq.py
@@ -2,7 +2,7 @@
 A module that provides a custom priority queue implementation
 for use in A* and similar algorithms.
 
-Version of Oct. 17, 2017, which contains
+Version of Oct. 18, 2017, 3:00 PM,  which contains
 an adjustment to handle duplicate priority values
 together with elements that don't implement
 the __lt__ method.
@@ -11,64 +11,53 @@ Ties are broken by adding a small random adjustment value
 to the new priority value.  These adjustments are less than
 0.00001.
 
+The Oct. 18 fixes the double-deletion error found
+in the Oct. 17 version.
+
  By S. Tanimoto,
 '''
 
 import heapq, random
 
+
 class PriorityQ:
-  def __init__(self):
-    self.elts = []
-    self.keys = {}
+    def __init__(self):
+        self.elts = []
+        self.keys = {}
+        self.priorities = {}
 
-  def isEmpty(self):
-    return len(self.elts)==0
+    def isEmpty(self):
+        return len(self.elts) == 0
 
-  def insert(self, element, priority):
-    try:
-      heapq.heappush(self.elts, (priority, element))
-    except TypeError as e:
-      # resolve priority ties by adding a small random increment to the new one.
-      adjustment = random.uniform(0.000001, 0.000009)
-      heapq.heappush(self.elts, (priority+adjustment, element))
-    self.keys[element]=True
+    def insert(self, element, priority):
+        if priority in self.priorities:
+            # resolve priority ties by adding a small random increment to the new one.
+            adjustment = random.uniform(0.000001, 0.000009)
+            priority += adjustment
+        self.priorities[priority] = True
+        heapq.heappush(self.elts, (priority, element))
+        self.keys[element] = True
 
-  def deletemin(self):
-    try:
-      item = heapq.heappop(self.elts)
-    except TypeError as e:
-      # A tie should never show up here, having been broken during the insert.
-      # but if does, this fix should work.  It did during some testing
-      # before the insertion tie-breaking was made.
-      item = self.elts[0]
-      del self.elts[0]
+    def deletemin(self):
+        item = self.elts[0]
+        (priority, element) = item
+        self.keys.pop(element)
+        self.priorities.pop(priority)
 
-    (priority, element) = item
+        try:
+            heapq.heappop(self.elts)
+        except TypeError as e:
+            # A tie should never show up here, having been broken during the insert.
+            # but if does, this fix should work.  It did during some testing
+            # before the insertion tie-breaking was made.
+            # del self.elts[0] # Commented out because heapq.heappop exception occurs after removal.
+            print("There was a TypeError in deletemin: " + str(e))
 
-    self.keys.pop(element, None)
-    return element
+        return element
 
-  def __contains__(self, element):
-    return element in self.keys
+    def __contains__(self, element):
+        return element in self.keys
 
-  def __str__(self):
-    return 'PriorityQ'+str(self.elts)
+    def __str__(self):
+        return 'PriorityQ' + str(self.elts)
 
-def test():
-  q = PriorityQ()
-  q.insert('A', 5)
-  q.insert('B', 4)
-  q.insert('C', 6)
-
-  print("B in Q is "+str('B' in q))
-  print("C in Q is "+str('C' in q))
-  print("D in Q is "+str('D' in q))
-
-  print(q)
-
-  print(q.deletemin())
-  print("B in Q is "+str('B' in q))
-  print(q.deletemin())
-  print(q.deletemin())
-
-if __name__ == "__main__": test()
