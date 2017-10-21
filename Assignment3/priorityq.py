0a714d835f5314aad2d868556dcd068bc8e54690
Vaibhavi <vaibhavi.rangarajan@gmail.com>
2017-10-21 09:07:34 -0700

    Answer for Part I , and made updated priority Q

--- Assignment3/priorityq.py
+++ Assignment3/priorityq.py
@@ -1,63 +1,40 @@
-'''priorityq.py
-A module that provides a custom priority queue implementation
-for use in A* and similar algorithms.
+'''This is an implementation of a priority queue that supports
+the remove operation as well as insert and deletemin.
+It is based on the heapdict implementation at
+https://github.com/DanielStutzbach/heapdict
+ S. Tanimoto, Oct. 20, 2017.
 
-Version of Oct. 18, 2017, 3:00 PM,  which contains
-an adjustment to handle duplicate priority values
-together with elements that don't implement
-the __lt__ method.
+This data structure is provided to support implementations
+of A* in Python.
 
-Ties are broken by adding a small random adjustment value
-to the new priority value.  These adjustments are less than
-0.00001.
-
-The Oct. 18 fixes the double-deletion error found
-in the Oct. 17 version.
-
- By S. Tanimoto,
 '''
-
-import heapq, random
+from heapdict import heapdict
 
 class PriorityQ:
-    def __init__(self):
-        self.elts = []
-        self.keys = {}
-        self.priorities = {}
+  def __init__(self):
+    self.h = heapdict()
 
-    def isEmpty(self):
-        return len(self.elts) == 0
+  def insert(self, elt, priority):
+    if elt in self.h:
+      raise Exception("Key is already in the priority queue: "+str(elt))
+    self.h[elt] = priority
 
-    def insert(self, element, priority):
-        if priority in self.priorities:
-            # resolve priority ties by adding a small random increment to the new one.
-            adjustment = random.uniform(0.000001, 0.000009)
-            priority += adjustment
-        self.priorities[priority] = True
-        heapq.heappush(self.elts, (priority, element))
-        self.keys[element] = True
+  def deletemin(self):
+    # Returns the element having smallest priority value.
+    return self.h.popitem()
 
-    def deletemin(self):
-        item = self.elts[0]
-        (priority, element) = item
-        self.keys.pop(element)
-        self.priorities.pop(priority)
+  def remove(self, elt):
+    # Removes an arbitrary element from the priority queue.
+    # This allows updating a priority value for a key by
+    # first removing it and then inserting it again with its
+    # new priority value.
+    del self.h[elt]  # invokes the __delitem__ method of heapdict.
 
-        try:
-            heapq.heappop(self.elts)
-        except TypeError as e:
-            # A tie should never show up here, having been broken during the insert.
-            # but if does, this fix should work.  It did during some testing
-            # before the insertion tie-breaking was made.
-            # del self.elts[0] # Commented out because heapq.heappop exception occurs after removal.
-            print("There was a TypeError in deletemin: " + str(e))
+  def __len__(self):
+    return len(self.h)
 
-        return element
+  def __contains__(self, elt):
+    return elt in self.h
 
-    def __contains__(self, element):
-        return element in self.keys
-
-    def __str__(self):
-        return 'PriorityQ' + str(self.elts)
-
-
+  def __str__(self):
+    return 'PriorityQ'+str(self.h.d)
